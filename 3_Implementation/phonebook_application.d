01f3594f9e029d08b7f51a034416246fa1d4eff2
Siddhartha Gupta <49288164+siddharth212@users.noreply.github.com>
2021-11-24 21:33:23 +0530

    Delete phonebook_application.d

--- 3_Implementation/phonebook_application.d
+++
@@ -1,3 +0,0 @@
-obj/phonebook_application.o: \
- C:\Users\siddhartha\Documents\M1_Application_Phonebook\3_Implementation\phonebook/phonebook_application.c \
- C:\Users\siddhartha\Documents\M1_Application_Phonebook\3_Implementation\phonebook/phonebook.h
